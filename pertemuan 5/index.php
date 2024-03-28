f3739c9e67a8119e8dd9377d2dbd6a31b77846ae
Sayid Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-03-28 15:29:23 +0700

    menambahkan folder pertemuan 5
    belajar php

---
+++ pertemuan 5/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skrip PHP pertama</title>
+</head>
+<body>
+    <?php 
+        echo "ini skrip PHP pertamaku!";
+    ?>
+</body>
+</html>
---
+++ pertemuan 5/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menggunakan Variabel</title>
+</head>
+<body>
+    <?php 
+
+        $jurusan = "Teknik Informatika";
+        echo ("Jurusan : $jurusan <br>");
+        $jurusan = "Sistem Informasi";
+        echo ("Jurusan : $jurusan <br>");
+
+    ?>
+</body>
+</html>
